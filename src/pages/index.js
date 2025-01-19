42e30387ad17b19e6679e8b0f2e49b141c949079
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2025-01-19 14:36:34 +0800

    Updated index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/">
-            Crabby Tutorial ⏱️
+            Get Started 📦
           </Link>
         </div>
       </div>
